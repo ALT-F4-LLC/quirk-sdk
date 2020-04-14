ba32aabc7472017b1ed19aa7c7f29a9ec340a1d9
Erik Reinert <e@drn.io>
2020-04-14 16:05:56 -0700

    Updates examples/websocket.ts for SocketIO replacement

--- examples/websocket.ts
+++ examples/websocket.ts
@@ -1,15 +1,15 @@
 import fetch from "node-fetch";
 import { exit } from "process";
-import websocket from "ws";
+import ws from "ws";
 
 async function main() {
-  const base = "https://websocket.quirk.gg";
+  const domain = "websocket.quirk.gg";
 
   const body = {
     auth_token: "<quirk-developer-token>"
   };
 
-  const response = await fetch(`${base}/token`, {
+  const response = await fetch(`https://${domain}/token`, {
     body: JSON.stringify(body),
     headers: { "Content-Type": "application/json" },
     method: "POST"
@@ -19,13 +19,15 @@ async function main() {
 
   const { token } = data;
 
-  const query = `?token=${token}&EIO=3&transport=websocket`;
+  const websocket = new ws(`wss://${domain}?token=${token}`);
 
-  const ws = new websocket(`${base}/socket.io/${query}`);
+  websocket.on("open", () => console.log("connected"));
 
-  ws.on("open", () => setInterval(() => ws.ping(), 25000));
+  websocket.on("error", console.log);
 
-  ws.on("message", console.log);
+  websocket.on("message", console.log);
+
+  websocket.on("close", console.log);
 }
 
 main().catch(e => {
